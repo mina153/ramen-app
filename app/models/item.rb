984c1caeaa68e49b1bcb413f8fa0bedb7d71c091
mina153 <popy0125@icloud.com>
2021-09-07 00:46:37 +0900

    new

---
+++ app/models/item.rb
@@ -0,0 +1,11 @@
+class Item < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :explanation, presence: true, length: { maximum: 200 }
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :city, presence: true
+ 
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+end
---
+++ db/migrate/20210906102319_create_items.rb
@@ -0,0 +1,12 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :title, null: false
+      t.text :explanation, null: false
+      t.integer :prefecture_id, null: false
+      t.string :city, null: false
+      t.references :user, null:false,foreign_key: true
+      t.timestamps
+    end
+  end
+end
